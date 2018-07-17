7c88f3587d440be19b5d78b35e97e8023bfea15b
mza921 <merill@ns3852643.ip-145-239-0.eu>
2018-07-17 11:35:57 -0700

    Fixed show list generation

--- plex2trakt.py
+++ plex2trakt.py
@@ -82,7 +82,7 @@ for search in config['search']:
     trakt_list_name = search['trakt_list_name']
     plex_library = plex.library.section(search['source_library'])
     log.info('%s: Gathering items from Plex.' % trakt_list_name)
-    if plex_library.type in ('movie', 'shows'):
+    if plex_library.type in ('movie', 'show'):
         if plex_library.type == 'movie':
             trakt_list_type = 'movies'
         elif plex_library.type == 'show':
@@ -91,10 +91,12 @@ for search in config['search']:
         for item in plex_library.search(**search['filters']):
             guid = urlparse(item.guid)
             log.debug('Adding: %s' % item.title)
-            # Looking for imdb, tmdb, or tvdb
+            # Looking for imdb, tmdb, or thetvdb
             id_type = guid.scheme.split('.')[-1]
-            if id_type in ('imdb', 'themoviedb', 'tvdb'):
+            if id_type in ('imdb', 'themoviedb'):
                 trakt_items[trakt_list_type].append({'ids': {id_type: guid.netloc}})
+            elif id_type in ('thetvdb'):
+                trakt_items[trakt_list_type].append({'ids': {'tvdb': guid.netloc}})
             else:
                 log.warning("Unknown agent for %s. Skipping." % item.title)
 
